0a5eac67202f9695f3dd751455dcf7cfa26b2727
Prakash Kumar Gupta <92747991+Prakash20210@users.noreply.github.com>
2023-07-12 02:14:14 +0530

    Shape with an abstract method

---
+++ ShapeCal.java
@@ -0,0 +1,87 @@
+import java.util.*;
+
+abstract class Shape
+ {
+    public abstract double calculateArea();
+}
+
+class Rectangle extends Shape
+ {
+
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width)
+     {
+
+        this.length = length;
+        this.width = width;
+    }
+
+    public double calculateArea()
+     {
+
+        return length * width;
+    }
+}
+
+class Circle extends Shape {
+
+    private double radius;
+
+    public Circle(double radius) {
+
+        this.radius = radius;
+    }
+
+    public double calculateArea() {
+
+        return Math.PI * radius * radius;
+    }
+}
+
+class Triangle extends Shape {
+
+    private double base;
+    private double height;
+
+    public Triangle(double base, double height)
+     {
+
+        this.base = base;
+        this.height = height;
+    }
+
+    public double calculateArea()
+     {
+        return 0.5 * base * height;
+    }
+}
+
+class ShapeCalculator {
+
+    public void printArea(Shape shape)
+     {
+        double area = shape.calculateArea();
+        System.out.println("The area of the shape is " + area);
+    }
+}
+
+public class ShapeCal 
+{
+    public static void main(String[] args)
+     {
+        Rectangle rectangle = new Rectangle(4, 6);
+        Circle circle = new Circle(3);
+        Triangle triangle = new Triangle(5, 8);
+
+        ShapeCalculator calculator = new ShapeCalculator();
+        calculator.printArea(rectangle); 
+        calculator.printArea(circle);    
+        calculator.printArea(triangle);  
+    }
+}
+
+
+
+
